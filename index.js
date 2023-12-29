aeace2a43cf4a1aceb57c8a416c305881c51fba3
JasGiigli <overview.jjj@gmail.com>
2023-12-29 02:50:15 -0800

    added html file

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 4000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Set the 'public' folder as the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the login form when a GET request is made to '/login'
+app.get('/login', (req, res) => {
+    // Your login form HTML content
+    const loginForm = `
+        <h1>Login Page</h1>
+        <form action="/login" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" value="Login">
+        </form>
+    `;
+    res.send(loginForm);
+});
+
+// Handling login POST request
+app.post('/login', (req, res) => {
+    // Here, you can handle the submitted login credentials
+    // For now, let's just send a confirmation message
+    res.send('Login request received');
+});
+
+// Catch-all route for other requests
+app.get('*', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
